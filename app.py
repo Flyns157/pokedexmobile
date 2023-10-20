bbf120ed50aecb31c50131cf2a83a236d2ec743e
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-20 17:27:25 +0200

    Mise en place du formulaire de requete à l'API vercel via la barre de recherche

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField
+import requests
 
 app = Flask(__name__, static_url_path='',
             static_folder='static',
             template_folder='templates')
 
+app.secret_key = "MSI c kro bien !"
 
-@app.route('/')
+class PokeNameForm(FlaskForm):
+    pokemon = StringField('nom du pokemon', validators=[DataRequired()])
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():  # put application's code here
+    poke_name_form = PokeNameForm()
+    if poke_name_form.validate_on_submit():
+        headers = {
+            "User-Agent": "RobotPokemon",
+            "From": "adresse[at]domaine[dot]com",
+            'Content-type': 'application/json'
+        }
+        return requests.get(f"https://api-pokemon-fr.vercel.app/api/v1/pokemon/{poke_name_form.pokemon.data}",headers)
     return render_template('index.html')
 
 
