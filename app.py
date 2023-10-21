521c6bfb545d2a9507893c2887dc121ecb524963
Cuisset Mattéo <matteo.cuisset@gmail.com>
2023-10-21 23:53:24 +0200

    quelques petits ajustements

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ app = Flask(__name__, static_url_path='',
 
 app.secret_key = "MSI c kro bien !"
 
-class PokeNameForm(FlaskForm):
-    pokemon = StringField('nom du pokemon', validators=[DataRequired()])
+class SearchForm(FlaskForm):
+    recherche = StringField('nom du pokemon', validators=[DataRequired()])
 
 if __name__ == '__main__':
     app.run()
@@ -33,16 +33,16 @@ if __name__ == '__main__':
 #=============================== MAIN ZONE ===============================
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    poke_name_form = PokeNameForm()
+    poke_name_form = SearchForm()
     if poke_name_form.validate_on_submit():
         search_result = search_engine.search_result(search_engine.search_engine(poke_name_form.pokemon.data))[0][0]
-        debug_sys.log('SEARCH',f'''{poke_name_form.pokemon.data} => {search_result}''')
+        debug_sys.log('SEARCH',f'''{poke_name_form.recherche.data} => {search_result}''')
         return redirect(url_for('pokemon_view', id = search_result))
     return render_template('index.html', form = poke_name_form)
 
 @app.route('/<id>')
 def pokemon_view(id):
-    poke_name_form = PokeNameForm()
+    poke_name_form = SearchForm()
     poke_infos = requests.get(f"{API_URL}/{id}",API_HEADER).json()
     debug_sys.log('INFO',str(poke_infos)[:100])
     try:
