b7eba949a83311ff8e9b3b056e5ddfb705e982ba
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-11-28 12:23:10 +0100

    update requiremets management

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+# Please write the contents of the Dockerfile to launch the application in a Docker container.
+Dockerfile:
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #=============================== IMPORTS ZONE ===============================
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for,abort
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 from wtforms import StringField
@@ -8,11 +8,12 @@ from flask_login import LoginManager,login_user, logout_user, login_required
 # personnal modules
 import search_engine
 search_engine.ECO = True
-import debug_sys
+from debug_sys.logger import Logger
 import User
 
 
 #=============================== INIT ZONE ===============================
+debug_sys = Logger()
 API_URL = "https://api-pokemon-fr.vercel.app/api/v1/pokemon"
 API_HEADER = {
     "User-Agent": "RobotPokemon",
@@ -42,8 +43,7 @@ def returnLogin():
     name1 = request.form['username']
     pwd = request.form['password']
     if name1 not in database or database[name1] != pwd:
-        return render_template('login.html',
-                                  info='Invalid User Or Password !')
+        return render_template('login.html',info='Invalid User Or Password !')
     else:
         return render_template('form_login.html')
 
@@ -77,32 +77,17 @@ def pokemon_view(id):
     try:
         if poke_infos['status'] == 404:
             debug_sys.log('404',f'''{poke_infos['message']} : "/{id}"''')
-        return f'''404 : {poke_infos['message']}"'''
+        return abort(404,poke_infos['message'])
     except:
         pass
     intitule = ['Vie','Attaque','Deffense','Attaque spéciale','Deffense spéciale','Vitesse']
     stats = [{'intitule': i, 'stat': s, 'value': v} for i, s, v in zip(intitule, poke_infos['stats'].keys(), poke_infos['stats'].values())]
     return render_template('pokemon_view.html', form = poke_name_form, poke_infos = poke_infos, stats=stats)
 
-
-#=============================== TEST ZONE ===============================
 from flask import request, jsonify
 
-@app.route('/test/<id>', methods=['GET'])
-def search_test(id):
-    poke_name_form = SearchForm()
-    poke_infos = search_engine.infos_on(id)
-    debug_sys.log('INFO',str(poke_infos))
-    try:
-        if poke_infos['status'] == 404:
-            debug_sys.log('404',f'''{poke_infos['message']} : "/{id}"''')
-        return f'''404 : {poke_infos['message']}"'''
-    except:
-        pass
-    return render_template('test_page.html', form = poke_name_form, poke_infos = poke_infos)
-
-@app.route('/search', methods=['GET'])
-def search():
+@app.route('/suggest', methods=['GET'])
+def suggest():
     query = request.args.get('query')
     suggestions = []
     for id in search_engine.search(query,'fr')[:MAX_SUGGESTION]:
@@ -110,31 +95,3 @@ def search():
         suggestions.append((information['name']['fr'],id,information['sprites']['regular']))
     debug_sys.log('SUGGEST', f'query={query} : ' + str(suggestions))
     return jsonify(suggestions)
-
-
-login = LoginManager(app)
-
-@login.user_loader
-def load_user(id):
-    return User.User.query.get(int(id))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        user = User.User.query.filter_by(username=request.form['username']).first()
-        if user is None or not user.check_password(request.form['password']):
-            return 'Invalid username or password'
-        login_user(user)
-        return 'Logged in successfully'
-    return render_template('login.html')
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return 'Logged out successfully'
-
-@app.route('/profile')
-@login_required
-def profile():
-    return 'Welcome to your profile!'
