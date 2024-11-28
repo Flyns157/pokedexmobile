146d8c4edf580ddeefd2c1ddd68d1c9b09fe0a57
Mattéo Cuisset <matteo.cuisset@gmail.com>
2024-11-28 14:51:12 +0100

    fix in init_db

--- pokedexmobile/utils/init_db.py
+++ pokedexmobile/utils/init_db.py
@@ -6,7 +6,7 @@ connection = sqlite3.connect('database.db')
 connection.executescript(
 """DROP TABLE IF EXISTS favoris;
 
-CREATE TABLE avoris (
+CREATE TABLE favoris (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     id_user INTEGER NOT NULL,
     id_pokemon INT NOT NULL
